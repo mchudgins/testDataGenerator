93df207768ad8f94fb545712a9bc24ea13bcb42d
Mike Hudgins <mchudgins@gmail.com>
2016-04-24 10:07:47 -0500

    changed strategy for ssn generation slightly

--- pkg/ssn/ssn.go
+++ pkg/ssn/ssn.go
@@ -13,17 +13,17 @@ import (
 */
 
 func GenerateInvalidSSN(r *rand.Rand) string {
-	switch r.Int31n(3) {
-	case 0:
+	switch r.Int31n(9) {
+	case 0, 1, 2: /* 6 random digits generated */
 		return fmt.Sprintf("000-%02d-%04d", r.Int31n(100), r.Int31n(10000))
 
-	case 1:
+	case 3, 4, 5, 6: /* 7 random digits generated */
 		return fmt.Sprintf("%03d-00-%04d", r.Int31n(1000), r.Int31n(10000))
 
-	case 2:
+	case 7, 8: /* 5 random digits generated */
 		return fmt.Sprintf("%03d-%02d-0000", r.Int31n(1000), r.Int31n(100))
 
-	default:
+	default: /* panic? */
 		return "000-00-0000"
 	}
 }
